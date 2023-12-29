eff1cff85e689cf187abff16b5edd587670371c4
ninad <ninad2eyes@gmail.com>
2023-12-29 15:16:09 -0500

    tryna make link to data on reddit work

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 print("HELLO BOSS")
 print("go kill youself")
+import os
+
+# os.environ["REDDIT_PASSWORD"] = "Anjali&Ninad1"
+# print(os.environ["REDDIT_PASSWORD"])
+os.system("SETX {0} {1} /M".format("REDDIT_PASSWORD", "\"Anjali&Ninad1\""))
