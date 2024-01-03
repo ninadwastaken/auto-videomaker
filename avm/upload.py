92bb41a6ae8119b02c0e50c4abd98f2be361dcae
ninad <ninad2eyes@gmail.com>
2024-01-03 15:38:12 -0500

    deletes created thumbnail :)

--- avm/upload.py
+++ avm/upload.py
@@ -1,20 +1,27 @@
 from instagrapi import Client
 import os
 
-def upload():
-    username = ""
-    password = ""
-    cl = Client()
-    cl.login(username, password)
+def upload(type, *args):
+    if type == "instagram":
+        instagram_upload(*args)
+    elif type == "youtube":
+        youtube_upload(*args)
+
 
 
 def instagram_upload(path:str, caption:str):
     # path needs to be absolute path for some reason
+
+    # env vars
     username = os.environ["INSTAGRAM_USERNAME"]
     password = os.environ["INSTAGRAM_PASSWORD"]
+
     cl = Client()
     cl.login(username, password)
     cl.clip_upload(path, caption)
+
+    # delete thumbnail
+    os.remove(f"{path}.jpg")
 
 def youtube_upload():
     # TODO! write upload for yt channel
@@ -22,4 +29,4 @@ def youtube_upload():
 
 
 if __name__ == '__main__':
-    instagram_upload(r"C:\Users\ninad\PycharmProjects\auto-video-maker\avm\media\1.mp4", "test1")
+    instagram_upload(r"C:\Users\ninad\PycharmProjects\auto-video-maker\avm\media\1.mp4", "test2")
